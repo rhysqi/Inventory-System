252935be8acf1656a75e5bea0f003f9a3a7fc97d
rhysqi <rhysqi.akbar@gmail.com>
2024-11-23 11:17:58 +0700

    Update: Viewmodel - Adding navigation service to viewmodel
    submit: rhysqi

---
+++ Apps/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Apps.ViewModels;
+
+public class BaseViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool>? _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler? CanExecuteChanged;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Apps/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Apps.Views;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Input;
+using Apps.Services;
+using Apps.Views.Pages;
+
+namespace Apps.ViewModels;
+
+class MainWindowViewModel : BaseViewModel
+{
+    public ICommand ItemsListPage {  get; }
+    public ICommand DeletedItemsPage{  get; }
+
+    private INavigationService _navigationService;
+
+    public MainWindowViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+
+        ItemsListPage = new RelayCommand(goToItemslist);
+        DeletedItemsPage= new RelayCommand(goToDeletedItems);
+    }
+
+    private void goToItemslist(object? parameter)
+    {
+        _navigationService.NavigateTo(new ItemsListPage());
+    }
+
+    private void goToDeletedItems(object? parameter)
+    {
+        _navigationService.NavigateTo(new DeletedItemsPage());
+    }
+}
