1a0e4690d5ba5866d2168c0369e5414147ffcea6
rhysqi <rhysqi.akbar@gmail.com>
2024-11-23 11:23:42 +0700

    Update: View - Update mainwindow
    submit: rhysqi

--- Apps/Views/MainWindow.xaml.cs
+++ Apps/Views/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using Apps.Services;
+using Apps.ViewModels;
+using Apps.Views.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Apps;
 
@@ -19,5 +13,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        var navigationService = new NavigationService(MainPage);
+
+        DataContext = new MainWindowViewModel(navigationService);
+
+        navigationService.NavigateTo(new ItemsListPage());
     }
 }
