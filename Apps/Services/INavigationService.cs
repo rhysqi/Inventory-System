604b2b1d42b924aeb875eef6fbe281472a03b0cd
rhysqi <rhysqi.akbar@gmail.com>
2024-11-23 11:17:31 +0700

    Adding: Service - Adding Navigation service
    submit: rhysqi

---
+++ Apps/Services/INavigationService.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+
+namespace Apps.Services;
+
+interface INavigationService
+{
+    void NavigateTo(Page page);
+}
---
+++ Apps/Services/NavigationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Apps.Services;
+
+internal class NavigationService : INavigationService
+{
+    private readonly Frame _frame;
+
+    public NavigationService(Frame frame)
+    {
+        _frame = frame;
+    }
+
+    public void NavigateTo(Page page)
+    {
+        if(page == null) throw new ArgumentNullException(nameof(page));
+        _frame.Navigate(page);
+    }
+}
