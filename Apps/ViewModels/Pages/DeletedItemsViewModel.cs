fc233de9d2071a01eb8527fb7e07ad4e7043e60b
rhysqi <rhysqi.akbar@gmail.com>
2024-11-28 17:42:20 +0700

    Update: ViewModels - Adding New Item Command - Update Add Item - Adding recover item - Adding recover all items
    submit: rhysqi

--- Apps/ViewModels/Pages/DeletedItemsViewModel.cs
+++ Apps/ViewModels/Pages/DeletedItemsViewModel.cs
@@ -2,6 +2,7 @@
 using Apps.Services;
 
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Apps.ViewModels.Pages;
@@ -14,7 +15,10 @@ internal class DeletedItemsViewModel : BaseViewModel
     public ICommand DeletedItemsGet { get; set; }
 
     public ICommand RecoverItemCommand { get; set; }
+    public ICommand RecoverAllItemCommand { get; set; }
+
     public ICommand DeleteItemCommand { get; set; }
+    public ICommand DeleteAllItemCommand { get; set; }
 
     public DeletedItemsViewModel()
     {
@@ -26,6 +30,10 @@ internal class DeletedItemsViewModel : BaseViewModel
         DeletedItemsGet = new RelayCommand(GetDeletedItems);
 
         RecoverItemCommand = new RelayCommand(RecoverItem);
+        RecoverAllItemCommand = new RelayCommand(RecoverAllItem);
+
+        DeleteItemCommand = new RelayCommand(DeleteItem);
+        DeleteAllItemCommand = new RelayCommand(DeleteAllItem);
     }
 
     private void GetDeletedItems(object parameter)
@@ -41,6 +49,7 @@ internal class DeletedItemsViewModel : BaseViewModel
         }
     }
 
+    // Recover deleted item
     private void RecoverItem(object parameter)
     {
         var itemToDelete = parameter as ItemsModel;
@@ -51,8 +60,82 @@ internal class DeletedItemsViewModel : BaseViewModel
 
             _itemsService.UpdateItem(itemToDelete);
 
-            RefreshItems();
         }
+        RefreshItems();
+        MessageBox.Show("Item are recovered", "Items Recovery");
+    }
+
+    // Recover all items command
+    private void RecoverAllItem(object parameter)
+    {
+        // Fetch all items that are deleted (IsDeleted = true)
+        var itemsList = _itemsService.GetAllItems()
+            .Where(items => items.IsDeleted)
+            .ToList();
+
+        if (itemsList.Count == 0)
+        {
+            MessageBox.Show("No deleted items found.");
+            return;
+        }
+
+        // Iterate through each item and set IsDeleted to false
+        foreach (var item in itemsList)
+        {
+            item.IsDeleted = false;
+            _itemsService.UpdateItem(item);
+        }
+
+        RefreshItems();
+        MessageBox.Show("All item are recovered", "Items Recovery");
+    }
+
+    // Delete item command
+    private void DeleteItem(object parameter)
+    {
+        var itemToDelete = parameter as ItemsModel;
+
+        string Msg = "Are you sure wanne delete permanent the item?";
+        var MsgBox = MessageBox.Show(Msg, "Permanent Delete Items", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+        if (MsgBox == MessageBoxResult.Yes)
+        {
+            if (itemToDelete != null)
+            {
+                _itemsService.DeleteItem(itemToDelete);
+            }
+        }
+
+        RefreshItems();
+    }
+
+    // Delete all items command
+    private void DeleteAllItem(object parameter)
+    {
+        // Fetch all items that are deleted (IsDeleted = true)
+        var itemsList = _itemsService.GetAllItems()
+            .Where(items => items.IsDeleted)
+            .ToList();
+
+        if (itemsList.Count == 0)
+        {
+            MessageBox.Show("No deleted items found.");
+            return;
+        }
+
+        string Msg = "Are you sure wanne delete permanent all deleted Items?";
+        var MsgBox = MessageBox.Show(Msg, "Permanent Delete All Items", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+        if (MsgBox == MessageBoxResult.Yes)
+        {
+            // Iterate and delete items that IsDeleted True
+            foreach (var item in itemsList)
+            {
+                _itemsService.DeleteItem(item);
+            }
+            MessageBox.Show("All item are deleted permanently");
+        }
+        RefreshItems();
     }
 
     private void RefreshItems()
--- Apps/ViewModels/Pages/ItemsListViewModel.cs
+++ Apps/ViewModels/Pages/ItemsListViewModel.cs
@@ -5,6 +5,7 @@ using System.Windows.Input;
 
 using Apps.Models;
 using Apps.Services;
+using Apps.Views.Windows;
 
 namespace Apps.ViewModels.Pages;
 
@@ -15,6 +16,7 @@ public class ItemsListViewModel : BaseViewModel
 
     public ICommand ItemsListGet { get; set; }
     public ICommand DeleteItemCommand {  get; set; }
+    public ICommand AddNewItemCommand { get; set; }
 
     public ItemsListViewModel()
     {
@@ -25,6 +27,7 @@ public class ItemsListViewModel : BaseViewModel
         ItemsListGet = new RelayCommand(GetItems);
 
         DeleteItemCommand = new RelayCommand(DeleteItem);
+        AddNewItemCommand = new RelayCommand(OpenAddItemWindow);
     }
 
     // Get full of items
@@ -42,7 +45,6 @@ public class ItemsListViewModel : BaseViewModel
     }
 
     private bool _isHandlingRowEdit = false;
-
     public void HandleRowEdit(ItemsModel item, DataGrid dataGrid)
     {
         if (_isHandlingRowEdit)
@@ -62,17 +64,7 @@ public class ItemsListViewModel : BaseViewModel
             if (item != null)
             {
                 var existingItem = Items.FirstOrDefault(x => x.ID == item.ID);
-
-                if (existingItem != null)
-                {
-                    // If item already in collection, Update Item
-                    UpdateItem(item);
-                }
-                else
-                {
-                    // If item not in collection, Add Item
-                    AddItem(item);
-                }
+                UpdateItem(item);
             }
         }
         finally
@@ -81,7 +73,22 @@ public class ItemsListViewModel : BaseViewModel
         }
     }
 
-    // Add data
+    // Function to open the AddItemWindow for adding a new item
+    private void OpenAddItemWindow(object parameter)
+    {
+        var addItemWindow = new AddNewItemWindow();
+
+        // Show the window and check if the user added an item
+        if (addItemWindow.ShowDialog() == true)
+        {
+            // Get the new item from the window
+            var newItem = addItemWindow.NewItem;
+
+            // Add the item to the collection and the database
+            AddItem(newItem);
+        }
+    }
+
     private void AddItem(object parameter)
     {
         var itemsAdd = parameter as ItemsModel;
@@ -95,21 +102,34 @@ public class ItemsListViewModel : BaseViewModel
                 return;
             }
 
-            // If ID not there, Add new Item
+            // If ID is not duplicated, add the new item
             var newItem = new ItemsModel
             {
                 ID = itemsAdd.ID,
                 Name = itemsAdd.Name,
                 Description = itemsAdd.Description,
+                Amount = itemsAdd.Amount,
                 IsDeleted = false,
                 DeletedDate = null
             };
 
-            // Add Item to database and collections
-            _itemsService.AddedItem(newItem);
-            Items.Add(newItem);
+            // Add Item to the database and collections
+            try
+            {
+                _itemsService.AddedItem(newItem); // Make sure this method adds the item to the database correctly
+                Items.Add(newItem); // Add to the ObservableCollection to update the UI
 
-            MessageBox.Show($"Added:\nID: {itemsAdd.ID}\nName: {itemsAdd.Name}\nDescription: {itemsAdd.Description}");
+                string Msg = $"Added Item:\nID: {newItem.ID}\nName: {newItem.Name}\nDescription: {newItem.Description}\nAmount: {newItem.Amount}";
+                MessageBox.Show(Msg, "Add New Item", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error adding item: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            MessageBox.Show("Invalid item data.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 
@@ -147,11 +167,10 @@ public class ItemsListViewModel : BaseViewModel
         if (itemToDelete != null)
         {
             itemToDelete.IsDeleted = true;
-
             _itemsService.UpdateItem(itemToDelete);
-
-            RefreshItems();
+            MessageBox.Show("Item is Deleted", "Delete Item");
         }
+        RefreshItems();
     }
 
     // Refresh Database
